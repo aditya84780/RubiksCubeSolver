22d1fa27aa8e6cde346cdfeeba80f4cb4df5a4c6
aditya84780 <adityasingh84780@gmail.com>
2024-06-18 21:09:32 +0530

    adding hash function RubiksCube3DArray

--- model/RubiksCube3DArray.cpp
+++ model/RubiksCube3DArray.cpp
@@ -351,4 +351,19 @@ public:
         this->b();
         return *this;
     }
+
+    struct hash3D {
+    public:
+        size_t operator()(const RubiksCube3DArray RB1) const {
+            string hasher = "";
+            for(int i = 0; i<6; i++) {
+                for(int j = 0; j<3; j++) {
+                    for(int k = 0; k<3; k++) {
+                        hasher += RB1.cube[i][j][k];
+                    }
+                }
+            }
+            return hash<string>()(hasher);
+        }
+    };
 };
